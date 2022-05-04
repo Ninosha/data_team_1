b536a07fa9bba85240f92c23b0b129ca2b9a6607
ninosha <nino.maridashvili.1@iliauni.edu.ge>
2022-05-04 18:25:26 +0400

    [update] metadata functionality

--- M3/W4/crawler/main.py
+++ M3/W4/crawler/main.py
@@ -6,76 +6,68 @@ module you can import, it can go to one's bigquery with credentials json
 
 import os
 from google.cloud import bigquery
-
-url = "/home/ninosha/Desktop/testing/DATA/nino-project-349013-" \
-      "8a8d680f044d.json"
-project_name = "nino-project"
 from pprint import pprint
+
+url = "/home/ninosha/Downloads/nino-project-349013-28e265e0c120.json"
+project_name = "nino-project"
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = url
 client = bigquery.Client()
-dataset = client.get_dataset("123")
+
 file_url = "/home/ninosha/Downloads/products.csv"
 table_id = "nino-project-349013.123.test_data"
 
-table = client.get_table(table_id)
-
-fields = [{"field_name": field.name,
-           "type": field.field_type,
-           "mode": field.mode,
-           "description": field.description}
-          for field in table.schema]
-
-res = {"creation_date": table.created, "last_update": table.modified,
-       "id": table_id, "description": table.description,
-       "location": table.location, "fields": fields,
-       "records": table.num_rows}
-
 datasets = client.list_datasets()
-job_config = bigquery.LoadJobConfig(
-    source_format=bigquery.SourceFormat.CSV,
-    skip_leading_rows=1
-)
-job = bigquery.job.QueryJob.job_id
-print(job)
-print(job_config)
+
 for dataset in datasets:
     dataset = client.get_dataset(dataset.dataset_id)
     dataset_meta = {"id": dataset.dataset_id,
                     "project": dataset.project,
-                    "created": dataset.created,
-                    "updated": dataset.modified,
+                    "created": dataset.created.strftime(
+                        "%m/%d/%Y, %H:%M:%S"),
+                    "updated": dataset.modified.strftime(
+                        "%m/%d/%Y, %H:%M:%S"),
                     "location": dataset.location,
                     "description": dataset.description}
 
     tables = client.list_tables(dataset.dataset_id)
 
     for table in tables:
+        print(table.table_id)
         table_full_id = f"{dataset.project}." \
                         f"{dataset.dataset_id}." \
                         f"{table.table_id}"
 
         table = client.get_table(table_full_id)
 
-        fields = [{"field_name": field.name,
-                   "type": field.field_type,
-                   "mode": field.mode,
-                   "description": field.description}
-                  for field in table.schema]
+        fields = [{
+            "field_name": field.name,
+            "type": field.field_type,
+            "mode": field.mode,
+            "description": field.description}
+            for field in table.schema]
 
-        res = {"creation_date": table.created,
-               "last_update": table.modified,
-               "id": table.full_table_id,
-               "description": table.description,
-               "location": table.location,
-               "fields": fields,
-               "records": table.num_rows}
+        table_meta = {"dataset": dataset.full_dataset_id,
+                      "creation_date": table.created.strftime(
+                          "%m/%d/%Y, %H:%M:%S"),
+                      "last_update": table.modified.strftime(
+                          "%m/%d/%Y, %H:%M:%S"),
+                      "id": table.full_table_id,
+                      "description": table.description,
+                      "location": table.location,
+                      "fields": fields,
+                      "records": table.num_rows,
+                      "size": table.__sizeof__(),
+                      "expiration_date": table.expires,
+                      "partitioning": table.partitioning_type,
+                      "partitioning_expiration": table.partition_expiration}
 
-# bigquery_service = bigquery.Client()
-# isnert = bigquery.jobs.inser()
-# dataset = bigquery_service.dataset("123")
 
-tables = client.list_tables("123")
-#
-# for table in tables:
-#     print(table.project, table.dataset_id, table_id, table.created)
+
+        # try:
+        #     parts = client.list_partitions(table)
+        #     print(f"{parts=}")
+        # except:
+        #     continue
+        pprint(dataset_meta)
+        pprint(table_meta)
